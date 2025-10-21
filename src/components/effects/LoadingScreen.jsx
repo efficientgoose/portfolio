0421dc08014d014c22fe59a0dd2866d936ff2d48
Ajinkya Kale <ajinkode@gmail.com>
2025-10-21 15:47:02 +0530

    fix: Loading animation

--- src/components/effects/LoadingScreen.jsx
+++ src/components/effects/LoadingScreen.jsx
@@ -5,7 +5,7 @@ export default function LoadingScreen() {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-950">
       <div className="w-64 h-64">
         <DotLottieReact
-          src="https://lottie.host/4ce08f38-14fa-4e39-862b-3999b8d6c62f/keL5ErArCV.lottie"
+          src="/loading-animation.lottie"
           loop
           autoplay
         />
